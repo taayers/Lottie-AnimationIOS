64cc14e78d92d98e2df857a5a1257d40e374c3ba
Todd Ayers <tayers@cardinalsolutions.com>
2019-02-25 08:09:44 -0500

    Initial Commit

---
+++ LottieAnimationTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LottieAnimationTest
+//
+//  Created by Todd Ayers on 2/25/19.
+//  Copyright © 2019 Todd Ayers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
